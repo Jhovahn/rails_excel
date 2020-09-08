cf4bcda6683b436f1ec98d54bc7971770c0adf92
_ <jhovahngibbs@Jhovahns-MacBook-Pro.local>
2020-09-07 20:26:14 -0700

    add user company relationship to company

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_005623) do
+ActiveRecord::Schema.define(version: 2020_09_08_020849) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_09_08_005623) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "company_id"
+    t.index ["company_id"], name: "index_users_on_company_id"
   end
 
 end
